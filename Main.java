2e5282369839bf26d314c8f5c2d09652b3fde664
Andrea <aforiglio@studenti.laziodigital.it>
2026-04-07 16:27:18 +0200

    aggiunto metodo printHello

--- Main.java
+++ Main.java
@@ -1,4 +1,9 @@
 public class Main{
+
+    public static void printHello(){
+        System.out.println("Hello");
+    }
+
     public static void main(String[] args){
         System.out.println("Hello World");
     }
