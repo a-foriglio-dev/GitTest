2c87a027a2407e09c93f3c06f58e96cbcaa231a0
Andrea <aforiglio@studenti.laziodigital.it>
2026-04-07 16:41:09 +0200

    Operations.java implementata

--- Main.java
+++ Main.java
@@ -6,5 +6,11 @@ public class Main{
 
     public static void main(String[] args){
         System.out.println("Hello World");
+        double x = 10.5;
+        double y = 5.2;
+        System.out.println(Operations.add(x, y));
+        System.out.println(Operations.subtract(x, y));
+        System.out.println(Operations.multiply(x, y));
+        System.out.println(Operations.divide(x, y));
     }
 }
